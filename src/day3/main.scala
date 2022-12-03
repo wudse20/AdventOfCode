798505b49be7e5aeb90af65d132720ca324300f3
Anton Skorup <anton@skorup.se>
2022-12-03 09:59:24 +0100

    Day3 part two

--- src/day3/main.scala
+++ src/day3/main.scala
@@ -2,17 +2,18 @@ type Path = String
 
 @main
 def main: Unit =
-  println(s"Part 1: ${part1}")
+  println(s"Part 1: $part1")
+  println(s"Part 2: $part2")
 
 def getFileContent(p: Path): Seq[String] = 
   import scala.io.Source
   Source.fromFile(p).getLines.toVector
 
-def part1: Int =
-  def getScore(c: String): Int =
-    if Character.isUpperCase(c(0)) then (c(0) - 'A').toInt + 27
-    else (c(0) - 'a') + 1
+def getScore(c: String): Int =
+  if Character.isUpperCase(c(0)) then (c(0) - 'A').toInt + 27
+  else (c(0) - 'a') + 1
 
+def part1: Int =
   val backpacks = getFileContent("./input.txt")
   var res: Int = 0
   for backpack <- backpacks do
@@ -21,4 +22,17 @@ def part1: Int =
     val second = backpack.split("").takeRight(len)
     res += getScore(first.intersect(second)(0))
 
+  res
+
+def part2: Int =
+  val backpacks = getFileContent("./input.txt")
+  var res: Int = 0
+  var i: Int = 0;
+  while i < backpacks.length do
+    var arr = backpacks(i).split("")
+    for ii <- 1 until 3 do
+      arr = arr.intersect(backpacks(i + ii).split(""))
+    
+    res += getScore(arr(0))
+    i += 3
   res
