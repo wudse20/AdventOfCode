fe1e9b76472641d3877a0a7925e5739a6332f296
Anton Skorup <anton@skorup.se>
2022-12-04 10:50:39 +0100

    Day4 part 2

--- src/day4/day4.lua
+++ src/day4/day4.lua
@@ -18,4 +18,27 @@ local function part1()
     return reductions
 end
 
+local function part2()
+    local input = SplitByNewLine(ReadAll("input.txt"))
+    local reductions = 0
+
+    for k, v in pairs(input) do
+        local line = SplitByComma(v)
+        local elf1 = { tonumber(SplitByHyphen(line[1])[1]), tonumber(SplitByHyphen(line[1])[2]) }
+        local elf2 = { tonumber(SplitByHyphen(line[2])[1]), tonumber(SplitByHyphen(line[2])[2]) }
+
+        if elf1[1] <= elf2[1] and elf1[1] >= elf2[2] then
+            reductions = reductions + 1
+        elseif elf1[1] >= elf2[1] and elf1[1] <= elf2[2] then
+            reductions = reductions + 1
+        elseif elf1[1] <= elf2[2] and elf1[2] >= elf2[1] then
+            reductions = reductions + 1
+        elseif elf1[1] >= elf2[2] and elf1[2] <= elf2[1] then
+            reductions = reductions + 1
+        end
+    end
+    return reductions
+end
+
 print("Part 1: " .. part1())
+print("Part 2: " .. part2())
