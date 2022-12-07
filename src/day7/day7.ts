ab4de485fa886c510c93a87c069c5384106c7874
Anton Skorup <anton@skorup.se>
2022-12-07 10:09:05 +0100

    Day 7 part 2

--- src/day7/day7.ts
+++ src/day7/day7.ts
@@ -1,63 +1,5 @@
+import { dir } from "console";
 import * as fs from "fs"
-
-function part1(): number {
-    let data: string = fs.readFileSync("./input.txt").toString();
-    let commands = data.split("\r\n");
-    let root = new Tree("/");
-    root.parrent = root;
-    let current = root;
-    let directories: Tree[] = [root];
-
-    for (let i = 0; i < commands.length; i++) {
-        let cmd = commands[i];
-
-        if (cmd[0] === "$") { // It is a command
-            let instruction = cmd.split(" ");
-
-            switch (instruction[1]) {
-                case "ls":
-                    while (i + 1 < commands.length && commands[i + 1][0] !== "$") { // Get all the items.
-                        i++;
-                        cmd = commands[i];
-                        let lineData = cmd.split(" ");
-
-                        if (lineData[0] === "dir") {
-                            let t = new Tree(lineData[1]);
-                            t.parrent = current;
-                            current.children.push(t);
-                            directories.push(t)
-                        } else {
-                            current.files.push(new File(+lineData[0], lineData[1]));
-                        }
-                    }
-                    break;
-                case "cd":
-                    let lineData = cmd.split(" ");
-
-                    if (lineData[2] === '/') {
-                        current = root;
-                        break;
-                    } else if (lineData[2] === '..') {
-                        current = current.parrent;
-                        break;
-                    }
-
-                    for (let i = 0; i < current.children.length; i++) {
-                        let currentChild = current.children[i];
-                        if (currentChild.name === lineData[2]) {
-                            current = currentChild;
-                            break;
-                        }
-                    }
-                    break;
-            }
-        }
-    }
-
-    return directories.map(t => t.size())
-        .filter(s => s < 100_000)
-        .reduce((acc, a) => acc + a);
-}
 
 class Tree {
     children: Tree[];
@@ -106,4 +48,89 @@ class File {
     }
 }
 
-console.log("Part 1: ", part1());
+const root = new Tree("/");
+root.parrent = root;
+const directories: Tree[] = [root];
+
+function buildTree() {
+    let data: string = fs.readFileSync("./input.txt").toString();
+    let commands = data.split("\r\n");
+    let current = root;
+
+    for (let i = 0; i < commands.length; i++) {
+        let cmd = commands[i];
+
+        if (cmd[0] === "$") { // It is a command
+            let instruction = cmd.split(" ");
+
+            switch (instruction[1]) {
+                case "ls":
+                    while (i + 1 < commands.length && commands[i + 1][0] !== "$") { // Get all the items.
+                        i++;
+                        cmd = commands[i];
+                        let lineData = cmd.split(" ");
+
+                        if (lineData[0] === "dir") {
+                            let t = new Tree(lineData[1]);
+                            t.parrent = current;
+                            current.children.push(t);
+                            directories.push(t)
+                        } else {
+                            current.files.push(new File(+lineData[0], lineData[1]));
+                        }
+                    }
+                    break;
+                case "cd":
+                    let lineData = cmd.split(" ");
+
+                    if (lineData[2] === '/') {
+                        current = root;
+                        break;
+                    } else if (lineData[2] === '..') {
+                        current = current.parrent;
+                        break;
+                    }
+
+                    for (let i = 0; i < current.children.length; i++) {
+                        let currentChild = current.children[i];
+                        if (currentChild.name === lineData[2]) {
+                            current = currentChild;
+                            break;
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
+
+function part1(): number {
+    return directories.map(t => t.size())
+                      .filter(s => s < 100_000)
+                      .reduce((acc, a) => acc + a);
+}
+
+const totalSize = 70000000;
+const requiredSpace = 30000000;
+
+function part2(): number {
+    let total: number = root.size();
+    let needed: number = totalSize - total; // The space we need to free up.
+    needed = requiredSpace - needed;
+    let res: number = totalSize; // Just starting with something big!
+
+    for (let i = 0; i < directories.length; i++) {
+        let current = directories[i];
+        let size = current.size();
+
+        if (size >= needed && size < res) {
+            res = size;
+        }
+    }
+
+    return res;
+}
+
+buildTree();
+console.log("Part 1:", part1());
+console.log("Part 2:", part2());
