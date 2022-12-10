01f564a2627542c7062799c86d0e41d7039a9b86
Anton Skorup <anton@skorup.se>
2022-12-10 09:40:34 +0100

    Day 10 part 2

--- src/day10/day10.c
+++ src/day10/day10.c
@@ -3,11 +3,18 @@
 #include <ctype.h>
 #include <string.h>
 
+#define LIT_PIXEL '#'
+#define DARK_PIXEL '.'
+
 long long part1(char *path, int lines);
+void part2(char* path, int lines);
+int drawPixel(int cycle, int x);
 
 int main()
 {
     printf("Part 1: %lld\n", part1("input.txt", 144));
+    printf("Part 2: \n");
+    part2("input.txt", 144);
 }
 
 long long part1(char *path, int lines)
@@ -100,4 +107,80 @@ long long part1(char *path, int lines)
     fclose(file);
     free(store);
     return res;
+}
+
+void part2(char* path, int lines)
+{
+    FILE *file = fopen(path, "r");
+
+    if (!file)
+    {
+        printf("\n Unable to open : %s ", path);
+        return;
+    }
+
+    char line[500];
+    int cycle = 0;
+    int indexCount = 0;
+    int waitNext = 0;
+    int x = 1;
+    int addNext = 0;
+
+    while (fgets(line, sizeof(line), file))
+    {
+        int len = strlen(line);
+        int i = 0;
+        char c = line[i];
+
+        if (waitNext)
+        {
+            cycle++;
+            printf("%c", drawPixel(cycle - 1, x) ? LIT_PIXEL : DARK_PIXEL);
+            
+            if (cycle % 40 == 0)
+                printf("\n");
+            
+            x += addNext;
+            addNext = 0;
+            waitNext = 0;
+        }
+
+        if (c == 'n')
+        {
+            waitNext = 0;
+        }
+        else
+        {
+            int sign = 1;
+            int value = 0;
+
+            i += 5;
+            if (line[i] == '-')
+                sign = -1;
+            else
+                i--;
+
+            while (i + 1 < len && isdigit(line[i + 1]))
+                value = value * 10 + line[++i] - '0';
+
+            value *= sign;
+            addNext = value;
+            waitNext = 1;
+        }
+
+        cycle++;
+
+        printf("%c", drawPixel(cycle - 1, x) ? LIT_PIXEL : DARK_PIXEL);
+        if (cycle % 40 == 0)
+            printf("\n");
+    }
+
+    fclose(file);
+}
+
+int drawPixel(int cycle, int pixel)
+{
+    return cycle % 40 == pixel - 1 ||
+           cycle % 40 == pixel     ||
+           cycle % 40 == pixel + 1;
 }
