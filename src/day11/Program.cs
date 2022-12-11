122125ab0d8f2c01fae0e363baa039cccb2096c0
Anton Skorup <anton@skorup.se>
2022-12-11 10:16:37 +0100

    Day 11 part 2

--- src/day11/Program.cs
+++ src/day11/Program.cs
@@ -4,7 +4,7 @@
     {
         private readonly List<Monkey> monkies = new();
 
-        private void prepData(String path)
+        private void PrepData(String path)
         {
             monkies.Clear();
             var input = File.ReadAllText(path)
@@ -22,7 +22,7 @@
                 var items = lines[1].Split(":")[1]
                                     .Split(",")
                                     .Select(s => s.Trim())
-                                    .Select(s => Int32.Parse(s))
+                                    .Select(s => Int64.Parse(s))
                                     .ToList();
 
                 var opStr = lines[2].Split("=")[1]
@@ -34,42 +34,42 @@
                 var t1 = opStr[0];
                 var t2 = opStr[2];
 
-                Func<int, int> operation = x => {
+                Func<long, long> operation = x => {
                     switch(op)
                     {
                         case "+":
-                            return (t1.Equals("old") ? x : Int32.Parse(t1)) + (t2.Equals("old") ? x : Int32.Parse(t2));
+                            return (t1.Equals("old") ? x : Int64.Parse(t1)) + (t2.Equals("old") ? x : Int64.Parse(t2));
                         case "-":
-                            return (t1.Equals("old") ? x : Int32.Parse(t1)) - (t2.Equals("old") ? x : Int32.Parse(t2));
+                            return (t1.Equals("old") ? x : Int64.Parse(t1)) - (t2.Equals("old") ? x : Int64.Parse(t2));
                         case "*":
-                            return (t1.Equals("old") ? x : Int32.Parse(t1)) * (t2.Equals("old") ? x : Int32.Parse(t2));
+                            return (t1.Equals("old") ? x : Int64.Parse(t1)) * (t2.Equals("old") ? x : Int64.Parse(t2));
                         case "/":
-                            return (t1.Equals("old") ? x : Int32.Parse(t1)) / (t2.Equals("old") ? x : Int32.Parse(t2));
+                            return (t1.Equals("old") ? x : Int64.Parse(t1)) / (t2.Equals("old") ? x : Int64.Parse(t2));
                         default:
                             throw new Exception("Illegal operation");
                     }
                 };
 
                 var condNum = Int32.Parse(lines[3].Split(" ")[3]);
-                Predicate<int> cond = x => x % condNum == 0;
+                Predicate<long> cond = x => x % condNum == 0;
 
                 var trueIndex = Int32.Parse(lines[4].Split(" ")[5]);
                 var falseIndex = Int32.Parse(lines[5].Split(" ")[5]);
 
-                monkies.Add(new Monkey(index, falseIndex, trueIndex, items, cond, operation));
+                monkies.Add(new Monkey(index, falseIndex, trueIndex, condNum, items, cond, operation));
             }
         }
 
-        private int Part1()
+        private long solution(int rounds, Func<long, long> worryUpdate)
         {
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < rounds; i++)
             {
                 foreach (var monkey in monkies)
                 {
                     foreach (var item in monkey.items.ToList())
                     {
                         monkey.count++;
-                        var newLevel = monkey.operation.Invoke(item) / 3;
+                        var newLevel = worryUpdate.Invoke(monkey.operation.Invoke(item));
                         var passTo = monkey.condition.Invoke(newLevel) ? monkey.trueIndex : monkey.falseIndex;
                         monkies[passTo].AddItem(newLevel);
                         monkey.RemoveItem(item);
@@ -77,20 +77,21 @@
                 }
             }
 
-            var pq = new PriorityQueue<int, int>();
+            var pq = new PriorityQueue<long, long>();
             foreach(var monkey in monkies)
-            {
-                Console.WriteLine("Monkey {0} inspected items {1} times", monkey.index, monkey.count);
                 pq.Enqueue(monkey.count, -monkey.count);
-            }
 
             return pq.Dequeue() * pq.Dequeue();
         }
 
         private void run()
         {
-            prepData("input.txt");
-            Console.WriteLine("Part 1: {0}", Part1());
+            PrepData("input.txt");
+            Console.WriteLine("Part 1: {0}", solution(20, x => x / 3));
+
+            PrepData("input.txt");
+            var mod = monkies.Aggregate(1, (mod, monkey) => mod * monkey.mod);
+            Console.WriteLine("Part 2: {0}", solution(10_000, x => x % mod));
         }
 
         public static void Main(String[] args) 
@@ -104,28 +105,33 @@
         public readonly int index;
         public readonly int falseIndex;
         public readonly int trueIndex;
-        public readonly List<int> items;
-        public readonly Predicate<int> condition;
-        public readonly Func<int, int> operation;
+        public int mod;
+        public readonly List<long> items;
+        public readonly Predicate<long> condition;
+        public readonly Func<long, long> operation;
 
-        public int count = 0;
+        public long count = 0;
 
-        public Monkey(int index, int falseIndex, int trueIndex, List<int> items, Predicate<int> condition, Func<int, int> operation)
+        public Monkey(
+            int index, int falseIndex, int trueIndex, int mod, List<long> items,
+            Predicate<long> condition, Func<long, long> operation
+        )
         {
             this.index = index;
             this.falseIndex = falseIndex;
             this.trueIndex = trueIndex;
+            this.mod = mod;
             this.items = items;
             this.condition = condition;
             this.operation = operation;
         }
 
-        public void AddItem(int item)
+        public void AddItem(long item)
         {
             items.Add(item);
         }
 
-        public void RemoveItem(int item)
+        public void RemoveItem(long item)
         {
             items.Remove(item);
         }
