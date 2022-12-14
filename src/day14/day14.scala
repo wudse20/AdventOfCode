42064d82fa86877b0b2e43cd594014ac299782a8
Anton Skorup <anton@skorup.se>
2022-12-14 09:24:26 +0100

    Day 14 part 2

--- src/day14/day14.scala
+++ src/day14/day14.scala
@@ -1,34 +1,32 @@
+import scala.collection.mutable
+
 @main
 def run: Unit =
   println(s"Part 1: $part1")
+  println(s"Part 2: $part2")
 
-def parseInput(p: String): Seq[Path] =
+def parseInput(p: String): mutable.Set[Path] =
   import scala.io.Source
-  import scala.collection.mutable.ArrayBuffer
-
   def strToPos(str: String): Pos =
    val split = str.split(",")
    Pos(split(0).toInt, split(1).toInt)
-
   val lines = Source.fromFile(p).getLines.toVector
-  val buf = ArrayBuffer.empty[Path]
-
+  val set = mutable.Set.empty[Path]
   for line <- lines do
     val l = line.split(" -> ")
                 .map(strToPos)
                 .toVector
-    buf += Path(l)
-
-  buf.toVector
+    set += Path(l)
+  set
 
 def part1: Int =
-  def simulateFall(p: Seq[Pos], sand: Pos): Option[Pos] =
+  def simulateFall(p: mutable.Set[Pos], sand: Pos): Option[Pos] =
     if !(p contains Pos(sand.x, sand.y + 1)) then Some(Pos(sand.x, sand.y + 1))
     else if !(p contains Pos(sand.x - 1, sand.y + 1)) then Some(Pos(sand.x - 1, sand.y + 1))
     else if !(p contains Pos(sand.x + 1, sand.y + 1)) then Some(Pos(sand.x + 1, sand.y + 1))
     else None
 
-  var paths: Vector[Pos] = parseInput("input.txt").map(_.all).flatten.toVector
+  val paths = parseInput("input.txt").map(_.all).flatten
   val lastRockY = paths.map(_.y).max
   var res = 0
   var current = Pos(500, 0)
@@ -36,8 +34,39 @@ def part1: Int =
   while shouldRun do
     simulateFall(paths, current) match
       case Some(x) => current = x
-      case _ => res += 1; paths = paths.appended(current); current = Pos(500, 0)
+      case _ => res += 1; paths += current; current = Pos(500, 0)
     shouldRun = !(current.y > lastRockY)
+  res
+
+def part2: Int =
+  def simulateFall(p: mutable.Set[Pos], sand: Pos, floorY: Int): Option[Pos] =
+    if !(p contains Pos(sand.x, sand.y + 1)) then Some(Pos(sand.x, sand.y + 1))
+    else if !(p contains Pos(sand.x - 1, sand.y + 1)) then Some(Pos(sand.x - 1, sand.y + 1))
+    else if !(p contains Pos(sand.x + 1, sand.y + 1)) then Some(Pos(sand.x + 1, sand.y + 1))
+    else None
+
+  val paths = parseInput("input.txt").map(_.all).flatten
+  val floor = paths.map(_.y).max + 2
+  var res = 0
+  var current = Pos(500, 0)
+  var shouldRun = true
+  while shouldRun do
+    simulateFall(paths, current, floor) match
+      case Some(x) =>
+        if x.y >= floor then
+          res += 1
+          paths += current
+          current = Pos(500, 0)
+        else
+          current = x
+      case _ =>
+        shouldRun = current != Pos(500, 0)
+        if shouldRun then
+          res += 1
+          paths += current
+          current = Pos(500, 0)
+        else
+          res += 1
   res
 
 case class Pos(x: Int, y: Int)
